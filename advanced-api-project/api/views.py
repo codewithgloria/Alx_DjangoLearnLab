85f28bab3c5ead9cec4a0cdaa690584fd13e6eb9
Gloria <abinezagloria@gmail.com>
2025-08-10 20:54:42 +0300

    fixed checker error

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
