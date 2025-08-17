10c210a2064d53827e61c341f91b7dc1204e555a
Gloria <abinezagloria@gmail.com>
2025-08-17 20:41:44 +0300

    added db configs to pass checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',        
+        'PASSWORD': '7962',    
+        'HOST': 'localhost',
+        'PORT': '5432',            
     }
 }
 
