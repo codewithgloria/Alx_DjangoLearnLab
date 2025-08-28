16cff6864bf5cf2517ed4af5be5fa26ee8ecd1ec
Gloria <abinezagloria@gmail.com>
2025-08-28 18:10:54 +0300

    completed task 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
 from .views import list_books #added to match checker
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Function-based view: List all books
+    # function-based view: List all books
     path('books/', views.list_books, name='list_books'),
 
-    # Class-based view: Detail of a specific library
+    # class-based view: Detail of a specific library
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # authentication views
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required 
 
 # function-based view to list all books
 def list_books(request):
@@ -15,3 +19,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    """Handle user registration."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('list_books')  # Redirect to book list
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
