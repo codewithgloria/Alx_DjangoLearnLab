28c5b7d58884d95aa9153789852f000899049cdd
Gloria <abinezagloria@gmail.com>
2025-09-17 01:42:20 +0300

    fixed checker error

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from relationship_app.models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    """View to list all books - requires can_view permission."""
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
